a0d5680dc63a851c34347d9e6aa7bdf3eaa0a058
Anagh Kumar Baranwal <anaghk.dos@gmail.com>
2015-03-24 03:51:00 +0530

    Update NASA-conn.py

--- NASA-conn.py
+++ NASA-conn.py
@@ -4,19 +4,29 @@
 # Date Last Modified : 24-03-2015
 # Authors : Anagh Kumar Baranwal, Manush Bhatt, Arnav Nag
 # License : GPLv3
+
 import MySQLdb
 
-db = MySQLdb.connect(host="localhost",user="admin",passwd="admin123",db="nasadb")
+def init():
+	db = MySQLdb.connect(host="localhost",user="admin",passwd="admin123",db="nasadb")
+	cur=db.cursor()
 
-cur=db.cursor()
+	sql = """CREATE TABLE STUDENT (
+	 	C_ID INT NOT NULL,
+	 	R_ID INT(10) NOT NULL,
+         	FIRST_NAME  CHAR(20) NOT NULL,
+         	LAST_NAME  CHAR(20),
+         	AGE INT,
+		PRIMARY KEY (R_ID) )"""
 
-sql = """CREATE TABLE STUDENT (
-	 C_ID INT NOT NULL
-	 R_ID INT(10) NOT NULL,
-         FIRST_NAME  CHAR(20) NOT NULL,
-         LAST_NAME  CHAR(20),
-         AGE INT
-	PRIMARY KEY (R_ID) )"""
+	cur.execute(sql)
+	db.close()
+	
+def add(values):
+	db = MySQLdb.connect(host="localhost",user="admin",passwd="admin123",db="nasadb")
+	cur=db.cursor()
 
-cur.execute(sql)
-db.close()
+	sql = """INSERT INTO STUDENT VALUES (VALUES)"""
+
+	cur.execute(sql)
+	db.close()
