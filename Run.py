fed35af5c612f246de6f88e5f2937491e5497074
Anagh Kumar Baranwal <anaghk.dos@gmail.com>
2015-03-30 07:18:17 +0530

    Created basic template for Connection

---
+++ Run.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+# NFC Attendance System
+# Date Last Modified : 24-03-2015
+# Authors : Anagh Kumar Baranwal, Manush Bhatt, Arnav Nag
+# Credits to : mxgxw
+# License : GPLv3
+
+import RPi.GPIO as GPIO
+import MFRC522
+import signal
+import NASA-conn as DB
+
+continue_reading = True
+
+# Capture SIGINT for cleanup when the script is aborted
+def end_read(signal,frame):
+    global continue_reading
+    print "Ctrl+C captured, ending read."
+    continue_reading = False
+    GPIO.cleanup()
+
+# Hook the SIGINT
+signal.signal(signal.SIGINT, end_read)
+
+# Create an object of the class MFRC522
+MIFAREReader = MFRC522.MFRC522()
+
+# Welcome message
+print "Welcome to the NASA read example"
+print "Press Ctrl-C to stop."
+
+# This loop keeps checking for chips. If one is near it will get the UID and authenticate
+while continue_reading:
+    
+    # Scan for cards    
+    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+
+    # If a card is found
+    if status == MIFAREReader.MI_OK:
+        print "Card detected"
+    
+    # Get the UID of the card
+    (status,uid) = MIFAREReader.MFRC522_Anticoll()
+
+    # If we have the UID, continue
+    if status == MIFAREReader.MI_OK:
+      # Check if value exists in table
+      DB.check(uid)
